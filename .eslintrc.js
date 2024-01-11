8777e67841bad87ecd40569c1857c6854dc5dc93
Nishan Bhandari <bnishan18@gmail.com>
2024-01-11 14:04:42 +0545

    PT-1: Eslint Setup Done

---
+++ .eslintrc.js
@@ -0,0 +1,54 @@
+module.exports = {
+  env: {
+    node: true,
+    commonjs: true,
+    es2021: true,
+  },
+  extends: ["airbnb-base", "plugin:prettier/recommended"],
+  overrides: [],
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  rules: {
+    "prettier/prettier": [
+      "error",
+      {
+        trailingComma: "es5",
+        semi: true,
+        printWidth: 80,
+      },
+    ],
+    "comma-dangle": [
+      "error",
+      {
+        arrays: "always-multiline",
+        objects: "always-multiline",
+        imports: "never",
+        exports: "never",
+        functions: "ignore",
+      },
+    ],
+    "no-param-reassign": [
+      "error",
+      {
+        props: false,
+      },
+    ],
+    quotes: [
+      "error",
+      "double",
+      {
+        avoidEscape: true,
+        allowTemplateLiterals: false,
+      },
+    ],
+    "no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
+    "linebreak-style": ["error", "unix"],
+    "eol-last": ["error", "always"],
+    "no-console": "error",
+  },
+};
