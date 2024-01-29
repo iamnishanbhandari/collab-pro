4ccb44655d9835bfe73bed1efa4044a6cb409262
Nishan Bhandari <bnishan18@gmail.com>
2024-01-30 00:00:02 +0545

    PT-4: Navabar and Home page added

--- src/App.js
+++ src/App.js
@@ -1,18 +1,12 @@
 import React from "react";
 import NavBar from "./components/landingpage/navbar/NavBar";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import LandingPage from "./components/landingpage/LandingPage";
 
 const App = () => {
   return (
     <div>
-      <Router>
-        <NavBar />
-        <Routes>
-          <Route path="">hello </Route>
-          <Route path="">hello </Route>
-          <Route path="">hello </Route>
-        </Routes>
-      </Router>
+      <LandingPage />
     </div>
   );
 };
