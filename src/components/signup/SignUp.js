8276721e3c6ef63f2b026375fc9d61bac80ca18a
Nishan Bhandari <bnishan18@gmail.com>
2024-02-03 23:58:32 +0545

    SignUp fixed

--- src/components/signup/SignUp.js
+++ src/components/signup/SignUp.js
@@ -2,6 +2,8 @@ import React, { useState } from "react";
 import { createUserWithEmailAndPassword } from "firebase/auth";
 import { auth } from "../Firebase/firebaseConfig";
 import { Link, useNavigate } from "react-router-dom";
+import light from "../../assets/light.png";
+import NavBar from "../landingpage/navbar/NavBar";
 
 const SignUp = () => {
   const [email, setEmail] = useState("");
@@ -41,42 +43,63 @@ const SignUp = () => {
 
   return (
     <>
-      <div>
-        <form onSubmit={onSubmit}>
-          <div>
-            <label>
-              Email
-              <input
-                type="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-              />
-            </label>
-          </div>
+      <NavBar />
+      {/* <div className="flex justify-evenly "> */}
+      <div className="border  flex flex-col justify-center  h-[100vh] ">
+        <div className="margin-auto pt-24  text-center border">
+          <p className="text-5xl text-yellow-600">
+            CONNECTING You TO The Right <br /> Voices
+          </p>
+        </div>
+        <div className=" h-500  w-96 m-auto mt-12">
+          <form onSubmit={onSubmit}>
+            <div className="">
+              <div className="text-red-800 p-2 ">Email</div>
+              <div>
+                <input
+                  type="email"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                  className="border border-solid border-gray-500 w-72 outline-none h-8 rounded-xl text-start p-4  "
+                />
+              </div>
+            </div>
 
-          <div>
-            <label>
-              Password
-              <input
-                type="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-              />
-            </label>
-          </div>
+            <div>
+              <div className="text-red-800 p-2">Password</div>
+              <div>
+                <input
+                  className="border border-solid border-gray-500 w-72 outline-none h-8 rounded-xl text-start p-4  "
+                  type="password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+              </div>
+            </div>
 
-          <div>
-            <button type="submit">Sign up</button>
-          </div>
+            <div className="">
+              <button
+                type="submit"
+                className="bg-blue-700 text-white border-red-799 w-36 rounded-xl h-8 mt-6"
+              >
+                Sign up
+              </button>
+            </div>
 
-          <div>
-            Have account ??
-            <Link to={"/signIn"}>
-              <button>sign in</button>
-            </Link>
-          </div>
-        </form>
+            <div className="mt-4">
+              Have account ??
+              <Link to={"/signIn"}>
+                <button>sign in</button>
+              </Link>
+            </div>
+          </form>
+        </div>
       </div>
+      {/* 
+        <div className="border border-green-600">
+          <img src={light} alt="image"></img>
+        </div>
+      </div> */}
     </>
   );
 };
