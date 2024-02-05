eab9746ed814b2789889f2f8c4b6909f084b24e5
Nishan Bhandari <bnishan18@gmail.com>
2024-02-05 23:55:28 +0545

    SideBar added

--- src/App.js
+++ src/App.js
@@ -4,17 +4,19 @@ import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import LandingPage from "./components/landingpage/LandingPage";
 import SignUp from "./components/signup/SignUp";
 import SignIn from "./components/signin/SignIn";
+import InfluencerDashboard from "./components/influencer/InfluencerDashboard";
 
 const App = () => {
   return (
     <>
-      <Router>
+      <InfluencerDashboard />
+      {/* <Router>
         <Routes>
           <Route path="/" element={<LandingPage />}></Route>
           <Route path="/SignUp" element={<SignUp />}></Route>
           <Route path="/SignIn" element={<SignIn />}></Route>
         </Routes>
-      </Router>
+      </Router> */}
     </>
   );
 };
